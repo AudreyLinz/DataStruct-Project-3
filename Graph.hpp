632d3eeb882815b4c8d082e6ab45118dbe6b95a8
Catherina Vue <cvue9082@floridapoly.edu>
2026-04-27 19:04:44 +0000

    got rid of all merge conflict headers

--- Graph.hpp
+++ Graph.hpp
@@ -1,55 +1,12 @@
-#ifndef GRAPH_HPP
-#define GRAPH_HPP
-
-#include <vector>
-#include <iostream>
-#include <map>
-#include <string>
-#include <utility> 
-
-struct Edge {
-    int neighbor;
-    int distance;
-    int cost;
-    Edge(int n, int d, int c) : neighbor(n), distance(d), cost(c) {}
-    bool operator<(const Edge& other) const { return distance < other.distance; }
-};
-
-template<typename T>
-class Graph {
-public:
-    void insertVertex(const T& value, const std::string& state);
-    void insertEdge(const T& v1, const T& v2, int dist, int cost);
-    
-    // Returns distance, cost for a direct edge between two nodes
-    std::pair<int, int> getEdgeWeights(const T& src, const T& dest) const;
-
-    //Finding shortest paths to state
-    std::pair<int, std::vector<std::vector<T>>> getShortestPathsToStateData(const T& src, const std::string& state) const;
-
-    //Finds the shortest path with exact number of stops
-    std::vector<T> shortestPathExactStops(const T& src, const T& dest, int stops, int& totalDist, int& totalCost);
-
-    // Finding shortest path between start and destination airports
-    std::pair<int, std::vector<T>> getShortestPathToAirport(const T& src, const T& dest);
-
-    //Finds the connectivity between airports
-    void displayAirportConnections() const;
-
-    //Creates an undirected graph
-    Graph<T> buildUndirectedGraph() const;
-
-    //Prim's Algorithem
-    void printMST() const;
-git rm --cached a.out
-private:
-    std::vector<T> vertices;
-    std::map<T, std::string> vertexToState; 
-    std::vector<std::vector<Edge>> edges;
-    
-    int getVertexIndex(const T& value) const;
-    void findPaths(int u, const std::vector<std::vector<int>>& parents, std::vector<T>& currentPath, std::vector<std::vector<T>>& allPaths) const;
-};
-
-#include "Graph.tpp"
-#endif
+main.cpp:179:22: warning: missing terminating ' character
+  179 | >>>>>>> a1e0730 (Prim's Algorithem Added)
+      |                      ^
+main.cpp:179:22: error: missing terminating ' character
+  179 | >>>>>>> a1e0730 (Prim's Algorithem Added)
+      |                      ^~~~~~~~~~~~~~~~~~~~
+In file included from main.cpp:7:
+Graph.hpp:44:1: error: ‘git’ does not name a type
+   44 | git rm --cached a.out
+      | ^~~
+In file included from Graph.hpp:54:
+Graph.tpp: In member function ‘int Graph<T>::getVertexIndex(const T&) const’:
